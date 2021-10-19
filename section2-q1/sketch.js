14a0184345f1404d2a22883e752cd14323c93199
Fjunna <2010575h@stu.kobe-u.ac.jp>
2021-10-19 12:21:26 +0900

    Change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,12 @@
 // 小手調べ
 function setup(){
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    if(i < 5) {
+      stroke(255,0,0);//red
+    }
+    else {
+      stroke(0,0,255)//blue
+    }// BLANK[1]
+    ellipse(50,50,100 - i * 10);//色を指定してから図形
   }
 }
