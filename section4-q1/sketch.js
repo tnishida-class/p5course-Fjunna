3f154c848b49c0017bb156863734bb302e4d1772
Fjunna <2010575h@stu.kobe-u.ac.jp>
2021-11-07 14:25:10 +0900

    Change section4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -7,18 +7,26 @@ function setup(){
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(20, 100); // 20以上100未満のランダムな数を代入
   }
 
   // 横線を引く
   const n = 10;
   for(let i = 0; i < n; i++){ line(0, height * i / n, width, height * i / n); }
 
-  // ここからが本番
+
   fill(0);
   const dx = width / scores.length;
-  let px, py; // 線を引くために一つ前の点を覚えておく変数
+  let px, py;
+  // 線を引くために一つ前の点を覚えておく変数
   for(let i = 0; i < scores.length; i++){
-    // BLANK[1]
+    const dy = scores[i] * height / 100
+      px = (i - 1 / 2) * dx;
+      py = scores[i - 1] * height / 100
+      strokeWeight(1);
+      line(px, py, (i + 1 / 2) * dx, dy);
+      strokeWeight(7);
+      point((i + 1 / 2) * dx, dy);
+
   }
 }
