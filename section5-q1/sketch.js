beb3f2b408d1986b04741d7ede7076f0cb2430ea
Fjunna <2010575h@stu.kobe-u.ac.jp>
2021-11-10 17:34:37 +0900

    Change section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,21 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  noStroke();
+  textSize(20);
+  balloon("I love ''The prince of tennis''.");
 }
 
 function balloon(t){
-  let w = textWidth(t);
-  let h = textAscent() + textDescent();
+  let w = textWidth(t);//吹き出しの幅
+  let h = textAscent() + textDescent();//吹き出しの高さ
   let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  fill(220, 255, 100);
+  ellipse(w, h + 1, h * 4/5);
   fill(255);
+  ellipse(w + 6, h + 1, h * 4/5 - 3);
+  fill(220, 255, 100);
+  rect(0, 0, w + p * 2, h + p * 2);
+  fill(255, 0, 0);
   text(t, p, h + p);
 }
